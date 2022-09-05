5d8e97db3fd4e4f746c0cf20d2afc66816c0c4da
yooonyng <yooonyng@gmail.com>
2022-09-05 16:36:36 +0900

    602

--- app/src/main/java/com/han/teamuzoo/adapter/FollowerAdapter.java
+++ app/src/main/java/com/han/teamuzoo/adapter/FollowerAdapter.java
@@ -55,7 +55,7 @@ public class FollowerAdapter extends RecyclerView.Adapter<FollowerAdapter.ViewHo
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            folRecyclerView = itemView.findViewById(R.id.folRecyclerView);
+            folRecyclerView = itemView.findViewById(R.id.fab);
             txtSuc = itemView.findViewById(R.id.txtSuc);
             txtDea = itemView.findViewById(R.id.txtDea);
             txtName = itemView.findViewById(R.id.txtName);
