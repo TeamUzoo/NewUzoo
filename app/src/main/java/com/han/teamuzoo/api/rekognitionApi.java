372816c7f9f7b6405d44db9f546162ab4b4d3eb8
jung hyun <102447800+Yeo616@users.noreply.github.com>
2022-09-13 14:56:28 +0900

    Rekognition

--- app/src/main/java/com/han/teamuzoo/api/rekognitionApi.java
+++ app/src/main/java/com/han/teamuzoo/api/rekognitionApi.java
@@ -13,40 +13,11 @@ import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
-public interface rekognitionApi {
+public interface RekognitionApi {
 
     @Multipart
     @POST("/object_detection")
     Call<RekogRes> rekognition(@Header("Authorization") String token,
-                              @Part MultipartBody.Part photo);
+                               @Part MultipartBody.Part photo);
 
-
-
-//      // 포스팅 생성 API
-//      @Multipart
-//      @POST("/posting")
-//              Call<PostRes> addPosting(@Header("Authorization") String token,
-//      @Part MultipartBody.Part photo,
-//      @Part("content") RequestBody content);
-//
-//    // 내 포스팅 리스트 가져오는 API
-//    @GET("/posting")
-//    Call<PostingList> getMyPosting(@Header("Authorization") String token,
-//                                   @Query("offset") int offset,
-//                                   @Query("limit") int limit);
-//
-//    // 친구들 포스팅 가져오는 API
-//    @GET("/posting/follow")
-//    Call<PostingList> getFollowPosting(@Header("Authorization") String token,
-//                                       @Query("offset") int offset,
-//                                       @Query("limit") int limit);
-//
-//    // 해당 포스팅에 좋아요 하는 API
-//    @POST("/like/{postingId}")
-//    Call<PostRes> setLike(@Header("Authorization") String token,
-//                          @Path("postingId") int postingId);
-//
-//    @DELETE("/like/{postingId}")
-//    Call<PostRes> unsetLike(@Header("Authorization") String token,
-//                            @Path("postingId") int postingId);
 }
